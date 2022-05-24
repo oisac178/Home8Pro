b6823dbcfd98a33604d05cf2de1d1c0a93fc82df
Alex <pf775_ash@rambler.ru>
2022-05-25 00:03:17 +0300

    "comment"

--- src/js/maps.js
+++ src/js/maps.js
@@ -5,16 +5,16 @@ ymaps.ready(init);
         // Порядок по умолчанию: «широта, долгота».
         // Чтобы не определять координаты центра карты вручную,
         // воспользуйтесь инструментом Определение координат.
-        center: [55.75846201917052,37.601120686508075],
+        center: [55.76045540677451,37.61475447613527],
         // Уровень масштабирования. Допустимые значения:
         // от 0 (весь мир) до 19.
-        zoom: 14.3
+        zoom: 13.3
       });
 
-      var myPlacemark = new ymaps.Placemark([55.75846201917052,37.601120686508075], {}, {
+      var myPlacemark = new ymaps.Placemark([55.76677091515023,37.63878531749671], {}, {
         iconLayout: 'default#image',
         iconImageHref: 'img/group68.svg',
-        iconImageSize: [20, 20],
+        iconImageSize: [15, 15],
         iconImageOffset: [-3, -42]
       });
       myMap.geoObjects.add(myPlacemark);
